0fd402e30989340c18a5112e2934a8a14e0a8513
IgorGepich <ihor.s@techspark.engineering>
2024-11-20 18:29:10 +0200

    WIP add sripts to execute
    Signed-off-by: IgorGepich <ihor.s@techspark.engineering>

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -4,16 +4,18 @@
 GREEN='\e[32m'
 RESET='\e[0m'
 
-echo -e "${GREEN}Starting the first script...${RESET}"
-# Запуск первого скрипта
-./first_script.sh
+echo -e "${GREEN}Starting the stream setup...${RESET}\n"
+./stream_setup.sh
 
-echo -e "${GREEN}First script completed. Starting the second script...${RESET}"
-# Запуск второго скрипта
-./second_script.sh
+echo -e "${GREEN}Stream setup completed. Starting the changing hostname...${RESET}\n"
+./change_hostname.sh
 
-echo -e "${GREEN}Second script completed. Starting the third script...${RESET}"
-# Запуск третьего скрипта
-./third_script.sh
+echo -e "${GREEN}Hostname changing completed. Starting access point setup...${RESET}\n"
+./rpi_access_point_setup.sh
 
-echo -e "${GREEN}All scripts completed successfully!${RESET}"
+echo -e "${GREEN}All scripts completed successfully!${RESET}\n"
+
+echo -e "${RED}Press any key to reboot...${RESET}"
+read -n 1 -s -r
+
+sudo reboot
