cbaa383ed27bccdf55ad363197e3e6bdf2500b9b
ihors-techspark <ihor.s@techspark.engineering>
2024-11-20 17:37:05 +0200

    add file stream_setup.sh
    Signed-off-by: ihors-techspark <ihor.s@techspark.engineering>

---
+++ stream_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+GREEN='\e[32m'
+RESET='\e[0m'
+
+echo -e "${GREEN}#########################################################################${RESET}"
+echo -e "${GREEN}#      THIS SCRIPT WILL SETUP MEDIAMTX ON RASPBERRY WITH RPI CAM        #${RESET}"
+echo -e "${GREEN}#########################################################################${RESET}"
+
+sleep 5
+
+echo -e "${GREEN}Press any key to continue...${RESET}"
+read -n 1 -s -r
+
+cd ..
+git clone https://github.com/IgorGepich/mediamtx.git -b mediamtx
+
+cd mediamtx
+
+wget https://raw.githubusercontent.com/IgorGepich/mediamtx/refs/heads/rpi_config/mediamtx.yml
+
+sudo chmod +x mediamtx
