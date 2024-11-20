7d599b242cf9c2a4879360e32c0c4e34ea6c7e8e
IgorGepich <ihor.s@techspark.engineering>
2024-11-20 20:50:29 +0200

    WIP design
    Signed-off-by: IgorGepich <ihor.s@techspark.engineering>

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
+GREEN='\e[32m'
+RED='\e[31m'
+RESET='\e[0m'
+
 set -e
+
+echo "${GREEN}#########################################################################${RESET}"
+echo "${GREEN}#      THIS SCRIPT WILL SETUP MEDIAMTX ON RASPBERRY WITH RPI CAM        #${RESET}"
+echo "${GREEN}#               CHANGE HOSTNAME FOR rtspserver.local                    #${RESET}"
+echo "${GREEN}#                      CREATE ACCESS POINT                              #${RESET}"
+echo "${GREEN}#########################################################################${RESET}"
+
 
 git clone --branch script https://github.com/IgorGepich/mediamtx.git
 
 cd mediamtx
+
+cd scripts
+
+rm -rf clone.sh
+
+cd ..
 
 mv scripts ..
 
