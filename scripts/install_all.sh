b7d8c855ca19e684c890875158f4928f7f9c8cc3
IgorGepich <ihor.s@techspark.engineering>
2024-11-21 13:22:31 +0200

    add logging function
    Signed-off-by: IgorGepich <ihor.s@techspark.engineering>

--- scripts/install_all.sh
+++ scripts/install_all.sh
@@ -1,29 +1,43 @@
 #!/bin/bash
 
-GREEN='\e[32m'
-RED='\e[31m'
-RESET='\e[0m'
+# Include logging functions
+source ./logging.sh
+
 SCRIPT_PATH=$(realpath "$0")
+LOGFILE="setup_main.log" # Log file for the main script
 
-echo -e "\n${GREEN}Starting the stream setup...${RESET}\n"
-./stream_setup.sh
+log_info "Starting the main setup process." | tee -a "$LOGFILE"
 
-echo -e "\n${GREEN}Stream setup completed. Starting the changing hostname...${RESET}\n"
-./change_hostname.sh
+log_info "Starting the stream setup script..." | tee -a "$LOGFILE"
+if ./stream_setup.sh 2>&1 | tee -a "$LOGFILE"; then
+    log_info "Stream setup completed successfully." | tee -a "$LOGFILE"
+else
+    log_error "Stream setup failed." | tee -a "$LOGFILE"
+    exit 1
+fi
 
-echo -e "\n${GREEN}Hostname changing completed. Starting access point setup...${RESET}\n"
-./rpi_access_point_setup.sh
+log_info "Starting the hostname change script..." | tee -a "$LOGFILE"
+if ./change_hostname.sh 2>&1 | tee -a "$LOGFILE"; then
+    log_info "Hostname changing completed successfully." | tee -a "$LOGFILE"
+else
+    log_error "Hostname change failed." | tee -a "$LOGFILE"
+    exit 1
+fi
 
-echo -e "\n${GREEN}All scripts completed successfully!${RESET}\n"
+log_info "Starting the access point setup script..." | tee -a "$LOGFILE"
+if ./rpi_access_point_setup.sh 2>&1 | tee -a "$LOGFILE"; then
+    log_info "Access point setup completed successfully." | tee -a "$LOGFILE"
+else
+    log_error "Access point setup failed." | tee -a "$LOGFILE"
+    exit 1
+fi
 
-echo -e "\n${RED}Press any key to reboot...${RESET}"
+log_info "All scripts completed successfully!" | tee -a "$LOGFILE"
 
-read -n 1 -s -r
-
+log_info "Deleting the main script: $SCRIPT_PATH..." | tee -a "$LOGFILE"
 (
     sleep 2
     rm -- "$SCRIPT_PATH"
 ) &
 
 cd ..
-
